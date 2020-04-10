a688b314440afa0408ccc51db6495d7abcbc361a
chip <chiposaur@gmail.com>
2020-04-10 14:00:35 -0500

    I hate it but whatever.

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -21,7 +21,15 @@ def get_indices_of_item_weights(weights, length, limit):
 	# 	if found != None:
 	# 		return (max(found, inhand), min(found, inhand))
 
-	
+	for i in range(length):
+		looking = limit - weights[i]
+		found = hash_table_retrieve(ht, looking)
+
+		if found == None:
+			hash_table_insert(ht, weights[i], i)
+		else:
+			return max(found, i), min(found, i)
+
 
 	return None
 
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,23 +1,35 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
-                        hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+						hash_table_insert,
+						hash_table_remove,
+						hash_table_retrieve,
+						hash_table_resize)
 
 
 class Ticket:
-    def __init__(self, source, destination):
-        self.source = source
-        self.destination = destination
+	def __init__(self, source, destination):
+		self.source = source
+		self.destination = destination
 
 
 def reconstruct_trip(tickets, length):
-    hashtable = HashTable(length)
-    route = [None] * length
+	ht = HashTable(length)
+	route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+	for t in tickets:
+		hash_table_insert(ht, t.source, t.destination)
 
-    pass
+	count = 0
+
+	prev = "NONE"
+	while count < length:
+		chk = hash_table_retrieve(ht, prev)
+		if chk == "NONE":
+			break
+		else:
+			route[count] = chk
+			prev = route[count]
+			count += 1
+	print(33, route[:-1])
+	return route[:-1]
+
